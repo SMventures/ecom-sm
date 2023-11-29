8975d0ca02b33848717221212a6f7769b9d1e202
kanchana37 <kanchanachopra376@gmail.com>
2023-11-29 14:55:49 +0530

    merged

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Count
-from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
 from . models import Product, Cart
@@ -7,7 +6,7 @@ from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 from . forms import LoginForm
 from .models import Customer
-from django.db.models import Q
+
 
 # Create your views here.
 def home(request):
@@ -53,46 +52,13 @@ class CustomerRegistrationView(View):
             messages.success(request,"Congratulations!User Registered Successfully")
         else:
             messages.warning(request,"Invalid Input Data")    
-
-     
-        
-        return redirect("login") 
-
-class ProfileView(View):
-    def  get(self,request):
-        form = CustomerProfileForm()
-        return render(request, 'app/profile.html',locals())
-    def  post(self,request):
-        form = CustomerProfileForm(request.POST)
-        if form.is_valid():
-            user = form.cleaned_data['user']
-            name = form.cleaned_data['name']
-            locality = form.cleaned_data['locality']
-            city = form.cleaned_data['city']
-            mobile = form.cleaned_data['mobile']
-            state = form.cleaned_data['state']
-            zipcode = form.cleaned_data['zipcode']
-            reg = Customer(user=user,name=name,locality=locality,city=city,mobile=mobile,state=state,zipcode=zipcode)
-            reg.save()
-            messages.success(request,"Congratulations! Profile Save Successfully")
-        else:
-            messages.warning(request,"Invalid Input Data")    
-            
-        return render(request, 'app/profile.html', locals())
-
-def address(request):
-    add = Customer.objects.filter(user=request.user)
-    print(add)
-    return render(request,'app/address.html',locals())   
-
-    # return render(request, "app/customerregistration.html",locals()) 
+        return render(request, "app/customerregistration.html",locals()) 
 
 class ProfileView(View):
     def  get(self,request):
         return render(request, "app/profile.html",locals())
     def  post(self,request):
         return render(request, "app/profile.html", locals())
-
     
 class updateAddress(View):   
     def get(self, request,pk):
@@ -128,89 +94,15 @@ def add_to_cart(request):
 def show_cart(request):
     user = request.user
     cart = Cart.objects.filter(user=user)
-    amount = 0
-    for p in cart:
-        value = p.quantity * p.product.discounted_price
-        amount = amount + value
-    totalamount = amount + 40
     return render(request, 'app/addtocart.html',locals())
-
-
-class checkout(View):
-    def get(self,request):
-        user=request.user
-        add=Customer.objects.filter(user=user)
-        return render(request,"app/checkout.html",locals())
-
-
-
-def plus_cart(request):
-    if request.method == 'GET':
-        prod_id=request.GET['prod_id']
-        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
-        c.quantity+=1
-        c.save()
-        user = request.user
-        cart = Cart.objects.filter(user=user)
-        amount = 0
-        for p in cart:
-            value = p.quantity * p.product.discounted_price
-            amount = amount + value
-        totalamount = amount + 40
-        # print(prod_id)
-        data={
-            'quantity':c.quantity,
-            'amount':amount,
-            'totalamount':totalamount
-        }
-        return JsonResponse(data)
-    
-    
-def minus_cart(request):
-    if request.method == 'GET':
-        prod_id=request.GET['prod_id']
-        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
-        c.quantity-=1
-        c.save()
-        user = request.user
-        cart = Cart.objects.filter(user=user)
-        amount = 0
-        for p in cart:
-            value = p.quantity * p.product.discounted_price
-            amount = amount + value
-        totalamount = amount + 40
-        # print(prod_id)
-        data={
-            'quantity':c.quantity,
-            'amount':amount,
-            'totalamount':totalamount
-        }
-        return JsonResponse(data)
-    
-def remove_cart(request):
-    if request.method == 'GET':
-        prod_id=request.GET['prod_id']
-        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
-        c.delete()
-        user = request.user
-        cart = Cart.objects.filter(user=user)
-        amount = 0
-        for p in cart:
-            value = p.quantity * p.product.discounted_price
-            amount = amount + value
-        totalamount = amount + 40
-        # print(prod_id)
-        data={
-            'amount':amount,
-            'totalamount':totalamount
-        }
-        return JsonResponse(data)
 
 class FAQsView(View):
     def get(self,request):
         return render(request,'app/FAQs.html',locals())
     
-
+class termsView(View):
+    def get(self,request):
+        return render(request,'app/Terms.html',locals())
 
     
     
